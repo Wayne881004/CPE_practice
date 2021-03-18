502fe3096e90866a2f9b2a5efc7262b1026af459
Wayne881004 <Wayne881004@gmail.com>
2021-03-18 18:23:28 +0800

    change name

---
+++ dataStructure/template.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+
+int main(int argc, char const *argv[]){
+    #ifndef ONLINE_JUDGE
+    freopen("input.in","r",stdin);
+    freopen("output.out","w",stdout);
+    #endif
+    return 0;
+}
